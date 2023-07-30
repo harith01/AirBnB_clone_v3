7bc38e26c2dc79e608d9ef8ab9646688ee463fac
harith01 <harithyusuph@gmail.com>
2023-07-30 15:02:24 +0100

    Handle 404 error

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,7 +2,7 @@
 """Create app"""
 
 from api.v1.views import app_views
-from flask import Flask, Blueprint
+from flask import Flask, Blueprint, make_response, jsonify
 import os
 from models import storage
 
@@ -14,6 +14,11 @@ app.register_blueprint(app_views)
 def teardown_appcontext(code):
     """Teardown Appcontext"""
     storage.close()
+
+@app.errorhandler(404)
+def not_found(error):
+    """Error handler"""
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 if __name__ == "__main__":
     app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
