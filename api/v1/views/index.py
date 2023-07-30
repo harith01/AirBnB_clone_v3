35802009d988c7c57478a0eaa850280e402c6235
harith01 <harithyusuph@gmail.com>
2023-07-30 14:52:19 +0100

    Stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,8 +3,27 @@
 
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 @app_views.route('/status', strict_slashes=False)
 def showStatus():
     """Show status"""
     return jsonify({"status": "OK"})
+
+@app_views.route('/stats', strict_slashes=False)
+def stats():
+    """Show number of objects by type"""
+    obj_count = {}
+    obj_count['amenities'] = storage.count(Amenity) 
+    obj_count['cities'] = storage.count(City) 
+    obj_count['places'] = storage.count(Place) 
+    obj_count['reviews'] = storage.count(Review) 
+    obj_count['states'] = storage.count(State) 
+    obj_count['users'] = storage.count(User)
+    return jsonify(obj_count)
